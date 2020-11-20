b5529c627d928e59109a211d8a5e561a8745b936
oney <1114303855@qq.com>
2020-11-20 17:16:12 +0800

    11-20

--- 代码练习/lstm_attention.py
+++ 代码练习/lstm_attention.py
@@ -1,6 +1,9 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+MAX_LENGTH = 3000
 
 class LSTMModel(nn.Module):
     def __init__(self):
@@ -8,100 +11,20 @@ class LSTMModel(nn.Module):
         """
         super().__init__()
         self.lstm = nn.LSTM(input_size=161, hidden_size=1024, num_layers=2, batch_first=True, dropout=0.4)
-        self.linear = nn.Linear(in_features=1024, out_features=161)
-        self.activation = nn.Sigmoid()
 
     def forward(self, ipt):
         self.lstm.flatten_parameters()
         o, h = self.lstm(ipt)
-        # o = self.linear(o)
-        # o = self.activation(o)
         return o , h
 
-
-if __name__ == '__main__':
-    ipt = torch.rand(1, 355, 161)
-    print(ipt.device)
-    opt = LSTMModel()(ipt)
-    print(opt.device)
-    print(opt.shape)
-
-
-
-class BiLSTM_Attention(nn.Module):
-
-    def __init__(self, vocab_size, embedding_dim, hidden_dim, n_layers):
-
-        super(BiLSTM_Attention, self).__init__()
-
-        self.hidden_dim = hidden_dim
-        self.n_layers = n_layers
-        self.embedding = nn.Embedding(vocab_size, embedding_dim)
-        self.rnn = nn.LSTM(embedding_dim, hidden_dim, num_layers=n_layers, bidirectional=True, dropout=0.5)
-        self.fc = nn.Linear(hidden_dim * 2, 1)
-        self.dropout = nn.Dropout(0.5)
-
-    # x,query：[batch, seq_len, hidden_dim*2]
-    def attention_net(self, x, query, mask=None):      #软性注意力机制（key=value=x）
-
-        d_k = query.size(-1)                                              #d_k为query的维度
-        scores = torch.matmul(query, x.transpose(1, 2)) / math.sqrt(d_k)  #打分机制  scores:[batch, seq_len, seq_len]
-
-        p_attn = F.softmax(scores, dim = -1)                              #对最后一个维度归一化得分
-        context = torch.matmul(p_attn, x).sum(1)       #对权重化的x求和，[batch, seq_len, hidden_dim*2]->[batch, hidden_dim*2]
-        return context, p_attn
-
-
-    def forward(self, x):
-        embedding = self.dropout(self.embedding(x))       
-        #[seq_len, batch, embedding_dim]
-
-        # output: [seq_len, batch, hidden_dim*2]     hidden/cell: [n_layers*2, batch, hidden_dim]
-        output, (final_hidden_state, final_cell_state) = self.rnn(embedding)
-        output = output.permute(1, 0, 2)                  
-        
-        #[batch, seq_len, hidden_dim*2]
-
-        query = self.dropout(output)
-        attn_output, attention = self.attention_net(output, query)       
-        #和LSTM的不同就在于这一句
-        
-        logit = self.fc(attn_output)
-        return logit
-
-
 class EncoderRNN(nn.Module):
-    def __init__(self, input_size, hidden_size):
-        super(EncoderRNN, self).__init__()
-        # self.hidden_size = hidden_size
+    def __init__(self):       # hidden_size = 1024   input_size=161
+        super().__init__()
+        self.lstm = nn.LSTM(input_size=161, hidden_size=1024, num_layers=2, batch_first=True, dropout=0.4)
 
-        # self.embedding = nn.Embedding(input_size, hidden_size)
-        self.gru = nn.GRU(input_size, hidden_size)
-
-    def forward(self, input, hidden):
-
-        output, hidden = self.gru(input, hidden)
-        return output, hidden
-
-    def initHidden(self):
-        return torch.zeros(1, 1, self.hidden_size, device=device)
-
-class DecoderRNN(nn.Module):
-    def __init__(self, hidden_size, output_size):
-        super(DecoderRNN, self).__init__()
-        self.hidden_size = hidden_size
-
-        self.embedding = nn.Embedding(output_size, hidden_size)
-        self.gru = nn.GRU(hidden_size, hidden_size)
-        self.out = nn.Linear(hidden_size, output_size)
-        self.softmax = nn.LogSoftmax(dim=1)
-
-    def forward(self, input, hidden):
-        output = self.embedding(input).view(1, 1, -1)
-        output = F.relu(output)
-
-        output, hidden = self.gru(output, hidden)
-        output = self.softmax(self.out(output[0]))
+    def forward(self, inp):
+        self.lstm.flatten_parameters()
+        output, hidden = self.lstm(inp)
         return output, hidden
 
     def initHidden(self):
@@ -111,6 +34,8 @@ class DecoderRNN(nn.Module):
 class AttnDecoderRNN(nn.Module):
     def __init__(self, hidden_size, output_size, dropout_p=0.1, max_length=MAX_LENGTH):
         super(AttnDecoderRNN, self).__init__()
+        
+        
         self.hidden_size = hidden_size
         self.output_size = output_size
         self.dropout_p = dropout_p
@@ -128,22 +53,23 @@ class AttnDecoderRNN(nn.Module):
 
         self.out = nn.Linear(self.hidden_size, self.output_size)
 
-    def forward(self, input, hidden, encoder_outputs):
-        embedded = self.embedding(input).view(1, 1, -1)
-        embedded = self.dropout(embedded)
+    def forward(self, inp, hidden, encoder_outputs):
+        # embedded = self.embedding(input).view(1, 1, -1)
+        # embedded = self.dropout(embedded)
 
         attn_weights = F.softmax(
-            self.attn(torch.cat((embedded[0], hidden[0]), 1)), dim=1)
+            self.attn(torch.cat((inp[0], hidden[0]), 1)), dim=1)
 
 
         attn_applied = torch.bmm(attn_weights.unsqueeze(0),
                                  encoder_outputs.unsqueeze(0))
 
-        output = torch.cat((embedded[0], attn_applied[0]), 1)
+        output = torch.cat((inp[0], attn_applied[0]), 1)
         output = self.attn_combine(output).unsqueeze(0)
 
         output = F.relu(output)
-        output, hidden = self.gru(output, hidden)
+
+        output, hidden = self.lstm(output, hidden)
 
         output = F.log_softmax(self.out(output[0]), dim=1)
         return output, hidden, attn_weights
@@ -151,3 +77,13 @@ class AttnDecoderRNN(nn.Module):
     def initHidden(self):
         return torch.zeros(1, 1, self.hidden_size, device=device)                
 
+if __name__ == '__main__':
+    ipt = torch.rand(1, 355, 161)
+
+    print(ipt.device)
+    # opt = LSTMModel()(ipt)
+    
+    encoder1 = EncoderRNN()(ipt)
+    attn_decoder1 = AttnDecoderRNN(hidden_size, 161, dropout_p=0.1)
+    print(attn_decoder1.shape)
+    # print(opt.shape)
