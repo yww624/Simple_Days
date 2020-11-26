e9391fd9a376b73c4164325c66883d36f5be6124
oney <1114303855@qq.com>
2020-11-26 16:12:34 +0800

    11_26

--- 代码练习/attention_pytorch.py
+++ 代码练习/attention_pytorch.py
@@ -272,3 +272,8 @@ def train(input_tensor, target_tensor, encoder, decoder, encoder_optimizer, deco
 
 
 
+hidden_size = 256
+encoder1 = EncoderRNN(input_lang.n_words, hidden_size).to(device)
+attn_decoder1 = AttnDecoderRNN(hidden_size, output_lang.n_words, dropout_p=0.1).to(device)
+
+trainIters(encoder1, attn_decoder1, 75000, print_every=5000)
